5575ab7e2e214ae586ce37f1611d978f1be37206
Georgiy <glnskkh@vk.com>
2022-09-07 12:51:10 +0500

    Empty RLE_jump

--- RLE/RLE_jump.js
+++ RLE/RLE_jump.js
@@ -5,7 +5,7 @@
 // @ts-check
 
 const COUNT_MIN = 3;
-const BYTE_MAX = 1 << 8 - 1;
+const BYTE_MAX = (1 << 8) - 1;
 const MUL_RANGE = BYTE_MAX / 2;
 const SINGLE_RANGE = BYTE_MAX - MUL_RANGE;
 
@@ -17,3 +17,34 @@ const SINGLE_RANGE = BYTE_MAX - MUL_RANGE;
 function RLE_encode(string) {
 
 }
+
+/**
+ * Decode text with RLE (jumping)
+ * @param {String} string
+ * @returns String
+ */
+function RLE_decode(string) {
+
+}
+
+
+const fs = require('fs');
+
+let type = process.argv[2];
+
+let is_encoding = type.startsWith('en');
+let is_decoding = type.startsWith('de');
+
+if (!is_encoding && !is_decoding) {
+  console.error("you should specify encode or decode mode");
+  process.exit(1);
+}
+
+const inputFile = process.argv[3];
+const outputFile = process.argv[4];
+
+let content = fs.readFileSync(inputFile, 'utf8');
+
+let result = is_encoding ? RLE_encode(content) : RLE_decode(content);
+
+fs.writeFileSync(outputFile, result, 'utf8');
